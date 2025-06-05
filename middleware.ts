d59d7b2ee863c0d1de6c3f53c27c41f6542d38f2
Amit Kukreja <thenameisamitkukreja@gmail.com>
2025-06-05 22:26:53 +0530

    Setup the Clerk and Minor Page Design and All

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,44 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher([
+  "/sign-in",
+  "/sign-up",
+  "/",
+  "/home",
+]);
+
+const isPublicApiRoute = createRouteMatcher(["/api/videos"]);
+export default clerkMiddleware(async (auth, req) => {
+  const { userId } = await auth();
+  const currentUrl = new URL(req.url);
+
+  const isAccessingDashboard = currentUrl.pathname === "/home";
+  const isApiRequest = currentUrl.pathname.startsWith("/api");
+
+  // If the user is logged in and accessing a public route but not the dashboard
+
+  if (userId && isPublicApiRoute(req) && !isAccessingDashboard) {
+    // Allow access to public API routes if the user is authenticated
+    return NextResponse.redirect(new URL("/home", req.url));
+  }
+  //not logged in
+  if (!userId) {
+    // If the user is not logged in and trying to access a protected route
+    if (!isPublicRoute(req) && !isPublicApiRoute(req)) {
+      // Add your logic here for unauthenticated users accessing public routes
+      return NextResponse.redirect(new URL("/sign-in", req.url));
+    }
+    //if the request is for a protected API and the user is not logged in
+    if (isApiRequest && !isPublicApiRoute(req)) {
+      // Redirect unauthenticated API requests to the home page
+      return NextResponse.redirect(new URL("/sign-in", req.url));
+    }
+  }
+  return NextResponse.next();
+});
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
-    "/(api|trpc)(.*)",
-  ],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  // matcher:    ["/((?!.*\\..*|_next).*)", "/", "/api/(.*)", "/trpc/(.*)"],
 };
